e2f2a486b81af129a5580ad069cf4c00746a664e
Guillaume Pellegrino <guillaume.pellegrino@yahoo.com>
2023-08-07 22:33:10 +0200

    Improve axis view

--- src/canvas.rs
+++ src/canvas.rs
@@ -29,14 +29,14 @@ impl<'a> Canvas<'a> {
         //
         // x_norm = (x - x_min) / (x_max - x_min)
         // x_pixel = x_norm * width
-        let x_view = self.view.x_max - self.view.x_min;
-        let x_norm = (x - self.view.x_min) / x_view;
+        let x_range = self.view.x_max - self.view.x_min;
+        let x_norm = (x - self.view.x_min) / x_range;
         x_norm * self.width
     }
 
     fn y_pixel(&self, y: f64) -> f64 {
-        let y_view = self.view.y_max - self.view.y_min;
-        let y_norm = (y - self.view.y_min) / y_view;
+        let y_range = self.view.y_max - self.view.y_min;
+        let y_norm = (y - self.view.y_min) / y_range;
         self.height - y_norm * self.height
     }
 
@@ -57,19 +57,71 @@ impl<'a> Canvas<'a> {
         self
     }
 
-    pub fn draw_axis(&self) -> &Self {
-        self.cairo.move_to(0.0, self.y_pixel(0.0));
-        self.cairo.line_to(self.width, self.y_pixel(0.0));
-
-        self.cairo.move_to(self.x_pixel(0.0), 0.0);
-        self.cairo.line_to(self.x_pixel(0.0), self.height);
-
-        /*
-        self.cairo.move_to(64.0, 64.0);
-        self.cairo.show_text("hello World");
-        for i in 0..10 {
+    /// Try to format float to a nice viewable string for user.
+    /// We try to take in account the user view:
+    /// - if user zoom in, we add more decimal
+    /// - if user zoom out, we remove unecessart decimal
+    fn fmtfloat(val: f64, view_range: f64) -> String {
+        let abs = view_range;
+        if abs > 10000.0 {
+            format!("{:.0}", val)
         }
-        */
+        else if abs > 1000.0 {
+            format!("{:.1}", val)
+        }
+        else if abs > 100.0 {
+            format!("{:.2}", val)
+        }
+        else if abs > 10.0 {
+            format!("{:.3}", val)
+        }
+        else if abs > 0.01 {
+            format!("{:.4}", val)
+        }
+        else if abs > 0.001 {
+            format!("{:.5}", val)
+        }
+        else if abs > 0.0001 {
+            format!("{:.6}", val)
+        }
+        else if abs > 0.00001 {
+            format!("{:.7}", val)
+        }
+        else {
+            format!("{:.8}", val)
+        }
+    }
+
+    pub fn draw_axis(&self) -> &Self {
+        let x0 = self.x_pixel(0.0);
+        let y0 = self.y_pixel(0.0);
+        let x_range = self.view.x_max - self.view.x_min;
+        let y_range = self.view.y_max - self.view.y_min;
+
+        // Draw X Axis
+        self.cairo.move_to(0.0, y0);
+        self.cairo.line_to(self.width, y0);
+        for i in 0..11 {
+            let i = i as f64;
+            let step = x_range / 10.0;
+            let start = (self.view.x_min / step).floor() * step;
+            let x = start + (i * step);
+            self.cairo.move_to(self.x_pixel(x), y0+10.0);
+            let _ = self.cairo.show_text(&Self::fmtfloat(x, x_range));
+        }
+
+        // Draw Y Axis
+        self.cairo.move_to(x0, 0.0);
+        self.cairo.line_to(x0, self.height);
+        for i in 0..10 {
+            let i = i as f64;
+            let step = y_range / 10.0;
+            let start = (self.view.y_min / step).floor() * step;
+            let y = start + (i * step);
+            self.cairo.move_to(x0-40.0, self.y_pixel(y));
+            let _ = self.cairo.show_text(&Self::fmtfloat(y, y_range));
+        }
+
         self.cairo.set_source_rgb(0.0, 0.0, 0.0);
         self.cairo.stroke()
             .expect("Cairo stroke failed");
